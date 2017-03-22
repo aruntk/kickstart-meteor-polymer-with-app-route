e035a73ce22e62b5e5fe430130fbda2b016e9c6e
aruntk <aruntknambiar@gmail.com>
2017-03-22 21:42:32 +0530

    no error version

--- imports/ui/components/test-element.js
+++ imports/ui/components/test-element.js
@@ -18,7 +18,8 @@ Polymer({
       value:true
     }
   },
-  behaviors:[mwcMixin],
+  // TODO Mixin does not work
+  // behaviors:[mwcMixin],
   showNickName:function(){
     this.nndHidden = !this.nndHidden;
     this.show = this.nndHidden ? "show" : "hide";
