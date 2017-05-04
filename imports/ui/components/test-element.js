cc51ce95f52df7ac19554de988565289ea82d6bf
aruntk <aruntknambiar@gmail.com>
2017-05-04 12:10:51 +0530

    elements converted to es6 class based format

--- imports/ui/components/test-element.js
+++ imports/ui/components/test-element.js
@@ -1,30 +1,31 @@
-Polymer({
-  is:"test-element",
-  properties:{
-    name:{
-      type:String,
-      value:"Arun Kumar"
-    },
-    nickname:{
-      type:String,
-      value:"tkay"
-    },
-    show:{
-      type:String,
-      value:"show"
-    },
-    nndHidden:{
-      type:Boolean,
-      value:true
-    }
-  },
-  // TODO Mixin does not work
-  behaviors:[mwcMixin],
-  showNickName:function(){
+class TestElement extends Polymer.Element {
+  static get is() {
+    return 'test-element';
+  }
+  static get properties() {
+    return {
+      name: {
+        type: String,
+        value: 'Arun Kumar',
+      },
+      nickname: {
+        type: String,
+        value: 'tkay',
+      },
+      show: {
+        type:String,
+        value: 'show',
+      },
+      nndHidden: {
+        type: Boolean,
+        value: true,
+      },
+    };
+  }
+  showNickName(){
     this.nndHidden = !this.nndHidden;
     this.show = this.nndHidden ? "show" : "hide";
   }
-
-})
-
+};
+window.customElements.define(TestElement.is, TestElement);
 
--- imports/ui/layouts/test-layout.js
+++ imports/ui/layouts/test-layout.js
@@ -1,37 +1,41 @@
-Polymer({
-  is:"test-layout",
-  behaviors:[mwcMixin],
-  properties:{
-    route:Object,
-    routeData:{
-      type: Object,
-      value: function() {
-        return {
-          page: ''
-        };
-      }
-    },
-    appState:{
-      type:String
-    },
-    notCordova:Boolean
+import { MwcMixin } from 'meteor/mwc:mixin';
 
-  },
-  trackers:["changeStatus(routeData.page)"],
-  changeStatus:function(page){
+class TestLayout extends MwcMixin(Polymer.Element) {
+  static get is() {
+    return 'test-layout';
+  }
+  static get properties() {
+    return {
+      route: Object,
+      routeData: {
+        type: Object,
+        value: function() {
+          return {
+            page: '',
+          };
+        },
+      },
+      appState: {
+        type: String,
+      },
+      notCordova: Boolean,
+    };
+  }
+  get trackers() {
+    return ['changeStatus(routeData.page)'];
+  }
+  changeStatus(page){
     this.set("appState",`Page : ${page || 'home'} , Status : ${Meteor.status().status}`);
     if(!Meteor.isCordova){
       this.notCordova = true;
     }
-  },
-
-  second:function(){
+  }
+  second(){
     this.set("routeData.page", "second"); 
-  },
-  home:function(){
-
+  }
+  home(){
     this.set("routeData.page", ""); 
   }
-});
+};
 
-
+window.customElements.define(TestLayout.is, TestLayout);
